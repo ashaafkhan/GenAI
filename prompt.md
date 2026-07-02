a61413c6ac14f872304d6c449b0492de5825d1c6
Ashaaf92 <ashaaf92@gmail.com>
2026-07-02 06:14:01 +0530

    implemented few-shot prompting

--- prompt.md
+++ prompt.md
@@ -2,3 +2,7 @@
 
 1. Zero Shot Prompting - Direct Instructions in one shot.
 
+2. Few Shot Prompting - Direct Instruction but with some examples outputs. This Influences the output.
+
+3.Chain Of Thought Prompting - 
+
--- prompting/02_few_shot.js
+++ prompting/02_few_shot.js
@@ -14,7 +14,16 @@ const client = new OpenAI({
 async function main() {
     const result = await client.chat.completions.create({
         model: 'gpt-4o-mini',
-        messages: [{ role: 'user', content: 'tell me whats 10+7' }],
+        messages: [{ role: 'user', 
+            content: `tell me whats 10+7?
+            don't add anything else in the ans,take the samples from the examples.
+            Examples:
+            - what is 5+4?
+             Expected Output: 9 (Nine)
+            - what is 10+10?
+             Expected Output: 20(Twenty)
+            `
+        }],
     });
     console.log(`Ans from OpenAI API:`,result.choices[0].message.content)
 }
